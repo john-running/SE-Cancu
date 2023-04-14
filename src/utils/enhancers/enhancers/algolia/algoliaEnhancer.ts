f8cb23fa85c07da22a70f6afe316ef908172c579
Alex Shyba <alex.shyba@uniform.dev>
2023-04-13 20:20:54 -0700

    readme and stuff

--- src/utils/enhancers/enhancers/algolia/algoliaEnhancer.ts
+++ src/utils/enhancers/enhancers/algolia/algoliaEnhancer.ts
@@ -1,12 +1,12 @@
 import { createEnhancer, AlgoliaClient } from '@uniformdev/canvas-algolia';
 
-export const algoliaConfigured: boolean = ![process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_SEARCH_KEY].includes(
+export const algoliaConfigured: boolean = ![process.env.NEXT_PUBLIC_ALGOLIA_APP_ID, process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY].includes(
   undefined
 );
 
 const client = new AlgoliaClient({
-  applicationId: process.env.ALGOLIA_APP_ID!,
-  searchKey: process.env.ALGOLIA_SEARCH_KEY!,
+  applicationId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
+  searchKey: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY!,
 });
 
 export const algoliaEnhancer = () =>
